6fdd58f80b1b7452b35681346a7904fecda319f5
gacordeiro <gacordeiro@gmail.com>
2023-03-28 10:34:24 -0300

    first commit

---
+++ maven_kotlin_erudio/src/main/kotlin/com/tutuland/erudio/MavenKotlinErudioApplication.kt
@@ -0,0 +1,11 @@
+package com.tutuland.erudio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MavenKotlinErudioApplication
+
+fun main(args: Array<String>) {
+    runApplication<MavenKotlinErudioApplication>(*args)
+}
---
+++ maven_kotlin_erudio/src/test/kotlin/com/tutuland/erudio/MavenKotlinErudioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tutuland.erudio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MavenKotlinErudioApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
